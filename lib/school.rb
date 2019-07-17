e450484f59318d34e55189fdb7c51e159eeab84c
John Odom <joodom91@gmail.com>
2019-07-17 17:39:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School
+
+    def initialize(name)
+        @name = name
+        @roster ={}
+    end
+    def name
+        @name
+    end
+    def roster
+        @roster
+    end
+   
+    def add_student(name, grade)
+            @roster[grade]||=[]
+            @roster[grade].push(name)
+    end
+    
+    def grade (grade)
+        return @roster[grade]
+    end
+end
